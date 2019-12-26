10bd051d262028da1fd3884a7ba807a10c1cd545
Kaentos <miguelmagueijo@hotmail.com>
2019-12-26 22:59:55 +0000

    add visual feedback to bat

--- setup.bat
+++ setup.bat
@@ -1,6 +1,25 @@
 @echo off
-title "Installing dependencies"
+title Installing dependencies
+
+echo Creating virtual environment...
 py -m venv venv
+echo Virtual environment created.
+
+echo.
+
+echo Activating virtual environment...
 call venv\Scripts\activate.bat
+echo Virtual environment activated
+
+echo.
+
+echo Installing requirements...
 pip install -r requirements.txt
+echo Requirements installed
+
+echo.
+
+echo Setup completed.
+pause
+
 deactivate
