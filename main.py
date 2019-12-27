2441ffbac192e79d13467ce17e47e2aeb2122da5
Kaentos <miguelmagueijo@hotmail.com>
2019-12-27 00:49:43 +0000

    URL validation part 2

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from __future__ import unicode_literals
 import youtube_dl
+import requests
 
 if __name__ == "__main__":
     ydl_opts = {
@@ -18,12 +19,15 @@ if __name__ == "__main__":
     print(URLs)
     
     # Validate URLs
-    URLs = [ url for url in URLs[1:] if "https://www.youtube.com/watch?v=" in url ]
+    URLs = [ url for url in URLs if "https://www.youtube.com/watch?v=" in url ]
+    testURL = "https://www.youtube.com/oembed?format=json&url="
+    URLs = [url for url in URLs if requests.get(f"{testURL}{url}").status_code == 200]
     print(URLs)
 
+    #https://www.youtube.com/oembed?format=json&url=https://www.youtube.com/watch?v= get json from youtube (use to validate)
 
     #with youtube_dl.YoutubeDL(ydl_opts) as ydl:
     #    ydl.download([''])
 
-    with open("musicURLs.txt", "w") as file:
-        file.write("> Give a new line for each link (link example: https://www.youtube.com/watch?v=Dqq2wXW3X2Q) <")
+    #with open("musicURLs.txt", "w") as file:
+    #    file.write("> Give a new line for each link (link example: https://www.youtube.com/watch?v=Dqq2wXW3X2Q) <")
