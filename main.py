d3c60fdccba5fc6630fe749ff8c62b6e6c156ea6
Kaentos <miguelmagueijo@hotmail.com>
2019-12-27 14:33:29 +0000

    add prints and remove comments

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-#import youtube_dl
 from youtube_dl import YoutubeDL
 import requests
 from configparser import ConfigParser
@@ -28,13 +27,12 @@ if __name__ == "__main__":
             if basicURL in url:
                 if requests.get(f"{testURL}{url}").status_code == 200:
                     validURLs.append(url)
-    print(validURLs)
-
-    #https://www.youtube.com/oembed?format=json&url=https://www.youtube.com/watch?v= get json from youtube (use to validate)
-
+    
+    print(f"URLs to download: {validURLs}")
     with YoutubeDL(ydl_opts) as ydl:
         for url in validURLs:
             ydl.download([url])
+    print("Download completed, check Mp3/.")
 
     with open("musicURLs.txt", "w") as file:
         file.write(Info["txt"]["example"])
