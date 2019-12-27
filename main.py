a8afebc6ad6250fbcc4bea6808cc85b96cc42b30
Kaentos <miguelmagueijo@hotmail.com>
2019-12-27 14:40:57 +0000

    create file if user deleted it by mistake

--- main.py
+++ main.py
@@ -1,10 +1,24 @@
 from youtube_dl import YoutubeDL
 import requests
 from configparser import ConfigParser
+from os import path
+
+def checkFile():
+    if not path.exists("musicURLs"):
+        print("musicURLs.txt not found, creating a new one...")
+        resetFile()
+        print("musicURLs.txt created, you can now input your music URLs there.")
+        exit()
+
+def resetFile():
+    with open("musicURLs.txt", "w") as file:
+        file.write(Info["txt"]["example"])
 
 if __name__ == "__main__":
     Info = ConfigParser()
     Info.read("info.ini")
+    
+    checkFile()
 
     ydl_opts = {
         "format": "bestaudio/best",
@@ -27,12 +41,14 @@ if __name__ == "__main__":
             if basicURL in url:
                 if requests.get(f"{testURL}{url}").status_code == 200:
                     validURLs.append(url)
-    
-    print(f"URLs to download: {validURLs}")
-    with YoutubeDL(ydl_opts) as ydl:
-        for url in validURLs:
-            ydl.download([url])
-    print("Download completed, check Mp3/.")
+    if validURLs:
+        print(f"URLs to download: {validURLs}")
+        with YoutubeDL(ydl_opts) as ydl:
+            for url in validURLs:
+                ydl.download([url])
+        print("Download completed, check Mp3/.")
+    else:
+        print("There are no videos to convert, did you input some in musicURLs.txt? Did you input them correctly?")
+        exit()
 
-    with open("musicURLs.txt", "w") as file:
-        file.write(Info["txt"]["example"])
+    resetFile()
