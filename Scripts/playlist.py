894a4b4989f10ceff5c22769ced95f9b2a23a70a
Kaentos <miguelmagueijo@hotmail.com>
2019-12-29 00:57:48 +0000

    error control

--- Scripts/playlist.py
+++ Scripts/playlist.py
@@ -6,7 +6,7 @@ from checkFolders import checkSinglePlaylist
 if __name__ == "__main__":
     Info = ConfigParser()
     Info.read("Data/info.ini")
-    
+
     checkSinglePlaylist()
 
     ydl_opts = {
@@ -23,10 +23,15 @@ if __name__ == "__main__":
             },
         ]
     }
-    
-    url = input("Playlist / Video from playlist URL: ")
-    if requests.get(f"{Info['yt']['checkLink']}{url}").status_code == 200:
-        if ("/watch?v=" in url and "&list=" in url) or "playlist?list=" in url:
-            with YoutubeDL(ydl_opts) as ydl:
-                ydl.download([url])
+    while True:
+        url = input("Playlist / Video from playlist URL: ")
+        if url.lower() == "exit":
+            exit()
+        if requests.get(f"{Info['yt']['checkLink']}{url}").status_code == 200:
+            if ("/watch?v=" in url and "&list=" in url) or "playlist?list=" in url:
+                with YoutubeDL(ydl_opts) as ydl:
+                    ydl.download([url])
+            break
+        else:
+            print("Invalid playlist URL. Try again. (CTRL+C to exit or type exit)")
     print("Playlist downloaded, check SinglePlaylist/.")
