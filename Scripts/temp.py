0abd96cb1463ba7aef678357493434c45d90e415
Miguel Magueijo <miguelmagueijo@hotmail.com>
2020-03-04 18:30:43 +0000

    add class and other changes to temp

--- Scripts/temp.py
+++ Scripts/temp.py
@@ -1,7 +1,34 @@
 import requests
 import os
+from youtube_dl import YoutubeDL
 
 ## This file will replace main.py
+
+class youtube_dlOptions:
+    def __init__(self):
+        self.isPlaylist = None
+        self.fileFormat = None
+
+    def getOptions(self):
+        options = {
+            "format": "bestaudio/best",
+            "outtmpl": "Mp3/%(title)s.%(ext)s",
+            "noplaylist": not self.isPlaylist,
+            "postprocessors": [
+                {
+                    "key": "FFmpegExtractAudio",
+                    "preferredcodec": self.fileFormat,
+                    "preferredquality": "192",
+                },
+                {
+                    'key': 'FFmpegMetadata'
+                },
+            ]
+        }
+        if self.fileFormat in "mp3":
+            options["writethumbnail"] = True
+            options["postprocessors"].append({ "key" : "EmbedThumbnail" })
+        return options
 
 def getOP():
     return input("Option: ")
@@ -14,7 +41,8 @@ def getVideoURL(alias):
             if checkURL(url):
                 return url
         elif len(url) == 11: # 11 = length of video id
-            if checkURL(f"www.youtube.com/watch?v={url}"):
+            url = f"www.youtube.com/watch?v={url}"
+            if checkURL(url):
                 return url
         elif url in alias["back"]:
             return None
@@ -27,7 +55,8 @@ def getPlaylistURL(alias):
             if checkURL(url):
                 return url
         elif len(url) == 34:
-            if checkURL(f"https://www.youtube.com/oembed?format=json&url=www.youtube.com/playlist?list={url}"):
+            url = f"https://www.youtube.com/oembed?format=json&url=www.youtube.com/playlist?list={url}"
+            if checkURL(url):
                 return url
         elif url in alias["back"]:
             return None
@@ -37,6 +66,13 @@ def checkURL(url):
         return True
     else:
         return False
+
+def downloadOne(url, ytdl_options):
+    print(ytdl_options)
+    print(url)
+    return
+    with YoutubeDL(ytdl_options) as ytdl:
+        ytdl.download([url])
 
 alias = { # Dict that stores alias for options
     ## type of conversion ##
@@ -62,37 +98,36 @@ alias = { # Dict that stores alias for options
 }
 
 audio_formats = ["MP3", "ACC", "FLAC", "M4A", "OPUS", "VORBIS", "WAV"]
-
+downloadOptions = youtube_dlOptions()
 
 while True: # Main loop
     print("1) Start converting\n2) Settings / Options\n0) Exit")
     op = getOP()
-    if op == "1": # youtube converter
-        print("1) Audio\n2) Video\n0) Back")
+    if op == "1": # Youtube Converter
         while op not in alias["back"]: # get file type (audio or video)
+            print("\n\n> File type:\n1) Audio\n2) Video\n0) Back")
             op = getOP()
             if op in alias["audio_type"]: # User choose audio type
-                print("Thumbnail only available to MP3 and M4A formats.")
+                print("\n\n> Audio format:\nThumbnail only available for MP3 and M4A formats.")
                 print("1) MP3\n2) ACC\n3) FLAC\n4) M4A\n5) OPUS\n6) VORBIS\n7) WAV\n0) Back")
                 while True:
                     op = getOP().lower()
                     if op not in audio_formats: # check if user inputed number
                         try:
                             selected_format = audio_formats[int(op) - 1].lower()
-                        except (ValueError, IndexError): # handle not numbers and index out of bounds
+                        except (ValueError, IndexError): # handle not int or index out of bounds
                             print("Invalid audio format.")
                             continue
                     else:
                         selected_format = op
-                    ## Set file ext/format in class
+                    downloadOptions.fileFormat = selected_format
                     break
             elif op in alias["video_type"]: # User choose video type
                 while True:
-                    print("Menu 2.2 / tipo de ficheiro desejado")
+                    print("Menu video")
                     op = getOP()
                     ## Set file ext/format in class
                     break
-
             elif op in alias["back"]: # Return to main menu
                 break
             else:
@@ -101,33 +136,38 @@ while True: # Main loop
 
             # Get option (1 video, >1 videos, 1 playlist, >1 playlist)
             if op not in alias["back"]:
-                print("What do you which to convert?")
-                print("1) Single video\n2) Multiple videos\n3) Single playlist\n4) Multiple playlists")
+                print("\n\n> What do you which to convert?\n1) Single video\n2) Multiple videos\n3) Single playlist\n4) Multiple playlists")
                 while True:
                     op = getOP()
                     if op in alias["1video"]:
+                        downloadOptions.isPlaylist = False
                         url = getVideoURL(alias)
                         if url:
+                            downloadOne(url, downloadOptions.getOptions())
                             pass #download
                         break
                     elif op in alias[">1video"]:
+                        downloadOptions.isPlaylist = False
                         ## open text file
                         break
                     elif op in alias["1playlist"]:
+                        downloadOptions.isPlaylist = True
                         url = getPlaylistURL(alias)
                         if url:
                             pass #download
                         break
                     elif op in alias[">1playlist"]:
+                        downloadOptions.isPlaylist = True
                         ## open text file
                         break
-
-            print(selected_format)
-            print(op)
-            print("Do you wish to exit? (y/n)")
-            op = getOP()
-            if op == "y" or op == "yes":
-                exit()
+                    
+            if op in alias["back"]:
+                break
+            else: 
+                print("Do you wish to exit? (y/n)")
+                op = getOP()
+                if op == "y" or op == "yes":
+                    exit()
 
 
 
