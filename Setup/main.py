b68a0028c7ee8cec4324182a680d4e6a1bd1a4c1
Kaentos <miguelmagueijo@hotmail.com>
2020-04-13 17:41:51 +0100

    rework try except, new value to info.ini

--- Setup/main.py
+++ Setup/main.py
@@ -5,29 +5,49 @@ try:
     import os
     import pathlib
 except ImportError:
-    raise ImportError("Please make sure you have installed all the packages in requirements.txt to the venv.")
+    print("Please make sure you have installed all the packages in requirements.txt to the venv.")
+    raise
 except BaseException:
-    raise BaseException("Error 1: check Data/errorList.txt.")
+    print("Error 1: check Data/errorList.txt.")
+    raise
 
 try:
     local_data = configparser.ConfigParser()
     local_data.read("Data/info.ini")
 except PermissionError:
-    raise PermissionError("You don't have permissions to read files.")
+    print("You don't have permissions to read files.")
+    raise
 except OSError:
-    raise OSError("Do you have Data/info.ini? If not please download from the original repository. https://github.com/Kaentos/pyYoutubeToMp3")
+    print("Do you have Data/info.ini? If not please download from the original repository. https://github.com/Kaentos/pyYoutubeToMp3")
+    raise
 except BaseException:
-    raise BaseException("Error 2: check Data/errorList.txt.")
+    print("Error 2: check Data/errorList.txt.")
+    raise
+
+try:
+    if local_data["user_setti2ngs"]["setup_concluded"] == True:
+        print("You already runned this script do you wish to run it again? (y/n)\nIt may cause problems and will probably give you lots of errors.")
+        op = input("Input: ").replace(" ", "")
+        if op in ["y", "yes"]:
+            local_data["user_settings"]["setup_concluded"] = False
+        else:
+            exit()
+except KeyError:
+    print("Please don't change Data/info.ini (if you don't what you are doing).\nTo fix this problem download info.ini from https://github.com/Kaentos/pyYoutubeToMp3.")
+    raise
 
 print("Creating necessary folders...", end=" ")
 try:
     path = f"{pathlib.Path().absolute()}/Downloads"
     os.mkdir(path)
 except PermissionError:
-    raise PermissionError("You don't have permissions to create folders here!")
+    print("You don't have permissions to create folders here!")
+    raise
 except OSError:
-    raise OSError(f"Downloads folder is already created. If you wish to run this script please delete {path}/.")
+    print(f"Downloads folder is already created. If you wish to run this script please delete {path}/.")
+    raise
 except BaseException:
-    raise BaseException("Error 3: check Data/errorList.txt.")
+    print("Error 3: check Data/errorList.txt.")
+    raise
 print("OK!")
 
